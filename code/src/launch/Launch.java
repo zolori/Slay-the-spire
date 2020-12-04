426eb293e5c620cf3e57cae99ee9699af46fe61a
alniemaz <alniemaz@iutclinfa2307l.iut.local>
2020-12-04 12:06:35 +0100

    [ADD] Ajout des classes de la vue, ajout d'un lien entre les fenetres Accueil et Salle

--- code/src/launch/Launch.java
+++ code/src/launch/Launch.java
@@ -1,10 +1,21 @@
 package launch;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Launch{
+
+public class Launch extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        FXML
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Accueil.fxml"));
+
+        primaryStage.setScene(new Scene(root, 700, 700));
+        primaryStage.setTitle("Accueil");
+        primaryStage.show();
+
     }
 }
---
+++ code/src/view/Acceuil.java
@@ -0,0 +1,48 @@
+package view;
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
+public class Acceuil extends Application {
+    @FXML
+    private Button lancer;
+    @FXML
+    private Button charger;
+    @FXML
+    private Button supprimer;
+
+
+    public void lancer(ActionEvent actionEvent) throws IOException {
+        Parent p = FXMLLoader.load(getClass().getResource("/Salle.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Salle");
+        stage.setScene(new Scene(p, 400, 600));
+        stage.show();
+        ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        stage.setFullScreen(true);
+
+    }
+    public void charger(ActionEvent actionEvent) {
+        charger.setText("Chargement de la partie !");
+    }
+    public void supprimer(ActionEvent actionEvent) {
+        supprimer.setText("Supression de la partie !");
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
