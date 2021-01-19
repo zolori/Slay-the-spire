02f535e56b7d234bbd4ff8bb4f808ebc8266454d
jeliot <jeliot@iutclinfb1909l.iut.local>
2021-01-19 19:58:05 +0100

    [Doc] Génération de la javadoc pour les vues

--- code/src/view/TaskCellFactory.java
+++ code/src/view/TaskCellFactory.java
@@ -5,8 +5,15 @@ import javafx.scene.control.ListView;
 import javafx.util.Callback;
 import model.Carte;
 
+/**
+ * TaskCell est la classe qui permet de charger les cartes de façon aléatoire
+ */
 public class TaskCellFactory implements Callback<ListView<Carte>, ListCell<Carte>> {
-
+    /**
+     * Cette fonction gère la création de TaskCell afin de créer de nouvelles cartes.
+     * @param param : ListView des cartes actuelles du joueur.
+     * @return Une nouvelle TaskCell qui va donc créer une nouvelle carte.
+     */
     @Override
     public ListCell<Carte> call(ListView<Carte> param) {
         return new TaskCell();
